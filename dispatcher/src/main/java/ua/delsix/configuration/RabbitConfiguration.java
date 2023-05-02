e7c7f3561478623cb0396195e0240cdbe1af25cf
delsix <delsixreborn@gmail.com>
2023-05-02 13:15:48 +0300

    docker-compose added

---
+++ dispatcher/src/main/java/ua/delsix/configuration/RabbitConfiguration.java
@@ -0,0 +1,26 @@
+package ua.delsix.configuration;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ua.delsix.RabbitQueue;
+
+@Configuration
+public class RabbitConfiguration {
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public Queue MESSAGE_UPDATE() {
+        return new Queue(RabbitQueue.MESSAGE_UPDATE);
+    }
+
+    @Bean
+    public Queue ANSWER_UPDATE() {
+    return new Queue(RabbitQueue.ANSWER_UPDATE);
+    }
+}
